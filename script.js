0e6adc8d5a1b3564061eaf9c0196d9b5eb796c15
khushi-singh-0515 <khushirajput0515@gmail.com>
2024-05-22 19:55:29 +0530

    my keyboard

---
+++ script.js
@@ -0,0 +1,26 @@
+// function tables(x){
+//     console.log(x)
+
+// }
+
+
+// function table(x){
+//     for(let i=1;i<=10;i++){
+//      tables(x*i)
+//     }
+// }
+// table(55)
+// let y=(x)=>{
+//     s(x)
+// }
+// function s(x){
+//     console.log(x)
+// } 
+// let table=(x)=>{
+//     for(let i=1;i<=10;i++){
+//        y(x*i) 
+//             }
+
+// }
+// table(60)
+let num=[2,3,4,5,6,8,9,7]
